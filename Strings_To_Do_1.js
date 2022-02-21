ec5d9dad84ceb2ad324851160b7d7693e1e08c81
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2022-02-20 23:07:29 -0600

    second commit

--- Strings_To_Do_1.js
+++ Strings_To_Do_1.js
@@ -3,10 +3,47 @@ var newString = oldString.replace(/\s+/g,"");
 
 console.log(newString);
 
+function removeBlanks(str) {
+    return str.split(" ").join("");
+}
+
+console.log(removeBlanks(" Pl ayTha tF u nkyM usi c "));
+
 var oldString = "0s1a3y5w7h9a2t4?6!8?0";
 var newString = oldString.replace(/\D/g,'');
 
 console.log(newString);
+
+function getDigits(str) {
+    var stringArr = str.split("");
+    var intStr = "";
+    for (var ind = 0; ind < stringArr.length; ind++) {
+        if (stringArr[ind]%1 == 0) {
+            intStr += stringArr[ind];
+        }
+    }
+    return intStr/1;
+}
+
+console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"));
+
+
+function acronyms(str) {
+    var strArr = str.split(" ");
+    var retStr = "";
+    for (var ind = 0; ind < strArr.length; ind++) {
+        if (strArr[ind] == false) {
+            continue;
+        } else {
+            retStr += strArr[ind][0].toUpperCase();
+        }
+    }
+    return retStr;
+}
+
+console.log(acronyms(" there's no free lunch - gotta pay yer way. "));
+console.log(acronyms( "Live from New York, it's Saturday Night!"));
+
 
 // echo "# strings_to_do_1" >> README.md
 // git init
