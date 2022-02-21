234bc76554123f0f93ee2db9fb24dfbf2e7af2b9
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2022-02-20 22:43:49 -0600

    first commit

---
+++ Strings_To_Do_1.js
@@ -0,0 +1,17 @@
+var oldString = "Pl ayTha tF u nkyM usi c";
+var newString = oldString.replace(/\s+/g,"");
+
+console.log(newString);
+
+var oldString = "0s1a3y5w7h9a2t4?6!8?0";
+var newString = oldString.replace(/\D/g,'');
+
+console.log(newString);
+
+// echo "# strings_to_do_1" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/jcambray10/strings_to_do_1.git
+// git push -u origin main
